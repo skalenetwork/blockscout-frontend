3cf248320b94d4fbf4081123890d8524e298ba49
Vadim Yavorsky <yavrsky@gmail.com>
2025-08-30 00:59:49 +0100

    Refactor getBlockReward to incorporate staticBlockReward from config

--- lib/block/getBlockReward.ts
+++ lib/block/getBlockReward.ts
@@ -1,13 +1,16 @@
 import BigNumber from 'bignumber.js';
 
 import type { Block } from 'types/api/block';
+import config from 'configs/app';
 
 export default function getBlockReward(block: Block) {
   const txFees = BigNumber(block.transaction_fees || 0);
   const burntFees = BigNumber(block.burnt_fees || 0);
-  const minerReward = block.rewards?.find(({ type }) => type === 'Miner Reward' || type === 'Validator Reward')?.reward;
-  const totalReward = BigNumber(minerReward || 0);
-  const staticReward = totalReward.minus(txFees).plus(burntFees);
+  const minerReward = BigNumber(block.rewards?.find(({ type }) => type === 'Miner Reward' || type === 'Validator Reward')?.reward || 0);
+  const staticReward = config.chain.staticBlockReward ?
+    BigNumber(config.chain.staticBlockReward) :
+    minerReward.minus(txFees).plus(burntFees);
+  const totalReward = staticReward.plus(txFees).minus(burntFees);
 
   return {
     totalReward,
